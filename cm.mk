f64d8cae04d7a282fb0734d14a81560c5dc60c00
kostoulhs <MrKostoulhs@gmail.com>
2015-12-01 02:36:40 +0200

    misc changes - add build fingerprint

--- cm.mk
+++ cm.mk
@@ -6,6 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/wilcoxltexx/full_wilcoxltexx.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte BUILD_FINGERPRINT="samsung/wilcoxltexx/wilcoxlte:4.4.2/KOT49H/G3815XXUCNH1:user/release-keys" PRIVATE_BUILD_DESC="wilcoxltexx-user 4.4.2 KOT49H G3815XXUCNH1 release-keys"
 
+PRODUCT_DEVICE := wilcoxltexx
 PRODUCT_NAME := cm_wilcoxltexx
--- wilcoxltexx.mk
+++ wilcoxltexx.mk
@@ -89,8 +89,8 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
+    $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
