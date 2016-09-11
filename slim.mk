3168791e8cfff262ad42ba87e604746d8af53443
Arne Coucheron <arco68@gmail.com>
2016-09-11 06:22:25 +0200

    Initial N bringup
    Change-Id: Ibca7f80a9422e49c5f7201de010bde86280867e9

--- slim.mk
+++ slim.mk
@@ -1,12 +1,33 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Copyright (C) 2016 The SlimRoms Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from wilcoxltexx device
+$(call inherit-product, device/samsung/wilcoxltexx/wilcoxltexx.mk)
 
 # Enhanced NFC
 #$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/wilcoxltexx/full_wilcoxltexx.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexx TARGET_DEVICE=wilcoxlte BUILD_FINGERPRINT="samsung/wilcoxltexx/wilcoxlte:4.4.2/KOT49H/G3815XXUCNH1:user/release-keys" PRIVATE_BUILD_DESC="wilcoxltexx-user 4.4.2 KOT49H G3815XXUCNH1 release-keys"
-
-PRODUCT_DEVICE := wilcoxltexx
 PRODUCT_NAME := slim_wilcoxltexx
+PRODUCT_DEVICE := wilcoxltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SM-G3815
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wilcoxltexxTARGET_DEVICE=wilcoxlte
