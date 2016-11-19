a5008ad2266819368ff978b0714eac70d1116495
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-19 07:34:06 +0100

    wilcoxltexx: Remove enhanced NFC feature
    # Conflicts:
    #	slim.mk

--- slim.mk
+++ slim.mk
@@ -18,9 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from wilcoxltexx device
 $(call inherit-product, device/samsung/wilcoxltexx/wilcoxltexx.mk)
 
-# Enhanced NFC
-#$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
-
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
